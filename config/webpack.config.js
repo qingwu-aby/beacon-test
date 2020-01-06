8e50954f7b50ffe7915e65566a71172779e70c6a
Yuan Yang <yuan.yang@shopee.com>
2020-01-06 21:02:52 +0800

    feat: list

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,6 +26,8 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const px2rem = require('postcss-px-to-viewport');
+// Gzip
+const CompressionPlugin = require('compression-webpack-plugin');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -564,6 +566,21 @@ module.exports = function(webpackEnv) {
             : undefined
         )
       ),
+      new CompressionPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+      new CompressionPlugin({
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: { level: 11 },
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358
---
+++ src/components/List/Item/VerticalItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const prefixCls = 'mall-common-item';
+const VerticalItem: React.FC = () => {
+  return <div>VerticalItem</div>
+}
+
+export default VerticalItem;
