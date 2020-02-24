913f3385cf54412146011c544d6dc993b0888512
Yuan Yang <yuan.yang@shopee.com>
2020-02-24 19:31:56 +0800

    g

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -9,32 +9,32 @@ import { mainList } from 'constants/menu'
 const BaseLayout: React.FC<any> = ({
   location
 }) => <Suspense fallback={<Loading />}>
-  <TransitionGroup className={'router-wrapper'}>
-    <CSSTransition
-      timeout={300}
-      classNames={'fade'}
-      key={location.pathname}
-      unmountOnExit={true}
-    >
+    <TransitionGroup className={'router-wrapper'}>
+      <CSSTransition
+        timeout={300}
+        classNames={'fade'}
+        key={location.pathname}
+        unmountOnExit={true}
+      >
 
-      <Switch location={location}>
-        <Route path='/home' component={MainLayout} />
-        {
-          mainList.map((item, index) => <Route
-            path={item.path}
-            exact={item.exact}
-            key={index}
-            component={item.comp}
-          />)
-        }
-        <Redirect
-          exact
-          from='/'
-          to='/home'
-        />
-      </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-</Suspense>;
+        <Switch location={location}>
+          <Route path='/home' component={MainLayout} />
+          {
+            mainList.map((item, index) => <Route
+              path={item.path}
+              exact={item.exact}
+              key={index}
+              component={item.comp}
+            />)
+          }
+          <Redirect
+            exact
+            from='/'
+            to='/home'
+          />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  </Suspense>;
 
 export default withRouter(BaseLayout);
